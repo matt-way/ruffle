0b6f9ec2d482887d7d57ce444d53a42ee5b053d3
Matt Way <matt__way@hotmail.com>
2015-07-05 20:36:36 +1000

    did new image downsampling

--- ionic/www/app/images/preprocess.js
+++ ionic/www/app/images/preprocess.js
@@ -4,70 +4,71 @@
 angular.module('ruffle.images.preprocess', [])
 	.service('ImagePreprocess', function($q){
 
-		// hermite downsampler
-		function resample_hermite(canvas, W, H, W2, H2){
-			var time1 = Date.now();
-			W2 = Math.round(W2);
-			H2 = Math.round(H2);
-			var img = canvas.getContext("2d").getImageData(0, 0, W, H);
-			var img2 = canvas.getContext("2d").getImageData(0, 0, W2, H2);
-			var data = img.data;
-			var data2 = img2.data;
-			var ratio_w = W / W2;
-			var ratio_h = H / H2;
-			var ratio_w_half = Math.ceil(ratio_w/2);
-			var ratio_h_half = Math.ceil(ratio_h/2);
-			
-			for(var j = 0; j < H2; j++){
-				for(var i = 0; i < W2; i++){
-					var x2 = (i + j*W2) * 4;
-					var weight = 0;
-					var weights = 0;
-					var weights_alpha = 0;
-					var gx_r = gx_g = gx_b = gx_a = 0;
-					var center_y = (j + 0.5) * ratio_h;
-					for(var yy = Math.floor(j * ratio_h); yy < (j + 1) * ratio_h; yy++){
-						var dy = Math.abs(center_y - (yy + 0.5)) / ratio_h_half;
-						var center_x = (i + 0.5) * ratio_w;
-						var w0 = dy*dy //pre-calc part of w
-						for(var xx = Math.floor(i * ratio_w); xx < (i + 1) * ratio_w; xx++){
-							var dx = Math.abs(center_x - (xx + 0.5)) / ratio_w_half;
-							var w = Math.sqrt(w0 + dx*dx);
-							if(w >= -1 && w <= 1){
-								//hermite filter
-								weight = 2 * w*w*w - 3*w*w + 1;
-								if(weight > 0){
-									dx = 4*(xx + yy*W);
-									//alpha
-									gx_a += weight * data[dx + 3];
-									weights_alpha += weight;
-									//colors
-									if(data[dx + 3] < 255)
-										weight = weight * data[dx + 3] / 250;
-									gx_r += weight * data[dx];
-									gx_g += weight * data[dx + 1];
-									gx_b += weight * data[dx + 2];
-									weights += weight;
-									}
-								}
-							}		
-						}
-					data2[x2]     = gx_r / weights;
-					data2[x2 + 1] = gx_g / weights;
-					data2[x2 + 2] = gx_b / weights;
-					data2[x2 + 3] = gx_a / weights_alpha;
-					}
+		function getContext (canvas){
+			var context = canvas.getContext('2d');
+
+			context.imageSmoothingEnabled = true;
+			context.mozImageSmoothingEnabled = true;
+			context.oImageSmoothingEnabled = true;
+			context.webkitImageSmoothingEnabled = true;
+
+			return context;
+		}
+
+		// resize by stepping down
+		function resizeStep(img, width, height, quality){
+			quality = quality || 1.0;
+
+			var resultD = $q.defer();
+			var canvas  = document.createElement('canvas');
+			var context = getContext(canvas);
+			var type = 'image/jpeg';
+
+			var cW = img.naturalWidth;
+			var cH = img.naturalHeight;
+
+			var dst = new Image();
+			var tmp = null;
+
+			function stepDown () {
+				cW = Math.max(cW / 2, width) | 0;
+				cH = Math.max(cH / 2, height) | 0;
+
+				canvas.width  = cW;
+				canvas.height = cH;
+
+				context.drawImage(tmp || img, 0, 0, cW, cH);
+
+				var url = canvas.toDataURL(type, quality);
+				
+				if(cW <= width || cH <= height){
+					return resultD.resolve(url);
 				}
-			console.log("hermite = "+(Math.round(Date.now() - time1)/1000)+" s");
-			canvas.getContext("2d").clearRect(0, 0, Math.max(W, W2), Math.max(H, H2));
-		    canvas.width = W2;
-		    canvas.height = H2;
-			canvas.getContext("2d").putImageData(img2, 0, 0);
+
+				dst.src = url;
+
+				if (!tmp) {
+					tmp = new Image();
+					tmp.onload = stepDown;
+				}
+
+				tmp.src = dst.src;
+			}
+
+			if(cW <= width || cH <= height || cW / 2 < width || cH / 2 < height){
+				canvas.width  = width;
+				canvas.height = height;
+				context.drawImage(img, 0, 0, width, height);				
+				resultD.resolve(canvas.toDataURL(type, quality));
+			} else {
+				stepDown();
+			}
+
+			return resultD.promise;
 		}
 
 		// resizes an image from data (assumes unit8array file data)
 		function resizeMaxWidth(dataUrl, maxWidth){
-			console.log('** starting preprocess');
 			var deferred = $q.defer();
 
 			var image = new Image();
@@ -77,23 +78,14 @@ angular.module('ruffle.images.preprocess', [])
 				var width = image.width;
 				var height = image.height;
 
-				var canvas = document.createElement('canvas');
-				var ctx = canvas.getContext('2d');
-
-				canvas.width = width;
-				canvas.height = height;
-
-				ctx.drawImage(image, 0, 0);
-
 				if(width > maxWidth){
-					var finHeight = height * (maxWidth / width);
-					var finWidth = maxWidth;
-					resample_hermite(canvas, width, height, finWidth, finHeight);
+					height = height * (maxWidth / width);
+					width = maxWidth;
 				}
 
-				var newUrl = canvas.toDataURL('image/jpeg', 0.9);
-				console.log('** preprocess complete');
-				deferred.resolve(newUrl);
+				resizeStep(image, width, height, 0.9).then(function(newUrl){
+					deferred.resolve(newUrl);
+				});
 			};
 
 			image.onerror = function(err){
